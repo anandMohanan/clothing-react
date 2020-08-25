6375700e746dd096687c598211152488948f77c1
ananth <ananthkvmohanan@gmail.com>
2020-08-25 20:47:35 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route , Link } from 'react-router-dom';
 
 import './App.css';
+import Header from  './components/header/header';
 import HomePage from './pages/homepage/homepage'; 
 import ShopPage from './pages/shop/shop'; 
 
@@ -9,6 +10,7 @@ import ShopPage from './pages/shop/shop';
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
     </div>
