bda76e4c759c952ecce2b9b91ce87aaaaa9fe1bf
ananth <ananthkvmohanan@gmail.com>
2020-10-08 13:11:59 +0530

    created a new backend server for payments

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
@@ -6,8 +7,21 @@ const StripeCheckoutButton = ({ price }) => {
     const publishableKey = 'pk_test_51HH2SuIJp0KzQRCijnmSJ1W1jRzDBKFX2ajPmPliUvDd87y646zHoIAHo2IDNjgKEXIQEfI4dAf3W7iM8rkvNSsF00riYwAnBf'
 
     const onToken = token => {
-            console.log(token);
-            alert("payment successfull")
+        axios({
+            url: 'payment',
+            method: 'post',
+            data:{
+                amount: priceForStripe,
+                token
+            }
+        })
+        .then(response => {
+            alert('payment was successfulll')
+        })
+        .catch(error => {
+            console.log(error)
+            alert('there was an isuue with your payment. Please use the test credit card details given')
+        })
     }
     
     return(
